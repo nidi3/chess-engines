47ebc1328470b09ab9b419e8726e019fa3dd58db
Stefan Niederhauser <ghuder5@gmx.ch>
2015-02-17 16:35:00 +0100

    initial commit

---
+++ src/main/java/guru/nidi/lpc/engines/Engines.java
@@ -0,0 +1,12 @@
+package guru.nidi.lpc.engines;
+
+import android.app.Application;
+
+/**
+ *
+ */
+public class Engines extends Application {
+    public Engines() {
+
+    }
+}
---
+++ src/main/java/guru/nidi/lpc/engines/MainActivity.java
@@ -0,0 +1,9 @@
+package guru.nidi.lpc.engines;
+
+import android.app.Activity;
+
+/**
+ *
+ */
+public class MainActivity extends Activity {
+}
